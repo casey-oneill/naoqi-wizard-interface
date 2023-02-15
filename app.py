ef13efd16fbd6e7e262b03ce999d4af3df3b14aa
Casey <53921484+casey-oneill@users.noreply.github.com>
2023-02-15 16:54:10 -0400

    WIP: Implementing introductions wizarding

--- app.py
+++ app.py
@@ -2,22 +2,23 @@ import time
 
 from nao_scripts import Nao
 
-from flask import Flask, request
+from flask import Flask, jsonify, request
 from flask import render_template
 
 NAO_IP = "192.168.2.251"
 NAO_PORT = 9559
 
-nao = Nao(NAO_IP, NAO_PORT)
+# nao = Nao(NAO_IP, NAO_PORT)
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == "POST":
-		action()
+		print(request.form['actionOne'])
+		# action()
 	return render_template('index.html')
 
-def action():
-	nao.stand()
-	time.sleep(1)
-	nao.sit()
+# def action():
+# 	nao.stand()
+# 	time.sleep(1)
+# 	nao.sit()
--- nao_scripts.py
+++ nao_scripts.py
@@ -11,11 +11,15 @@ class Nao:
 	def __init__(self, nao_ip, nao_port):
 		self.motion = ALProxy("ALMotion", nao_ip, nao_port)
 		self.posture = ALProxy("ALRobotPosture", nao_ip, nao_port)
+		self.tts = ALProxy("ALTextToSpeech", nao_ip, nao_port)
 		self.motion.wakeUp()
 		time.sleep(0.5)
 		self.posture.goToPosture("Sit", 0.5)
 		time.sleep(0.5)
 		self.motion.rest()
+
+	def say(self, s):
+		self.tts.say("\\rspd=110\\\\vct=125\\\\vol=80\\{}".format(s))
 
 	def stand(self):
 		self.motion.wakeUp()
@@ -26,3 +30,8 @@ class Nao:
 		self.motion.wakeUp()
 		self.posture.goToPosture("Sit", 0.5)
 		self.motion.rest()
+
+	def introduction(self, i, j):
+		with open('data/introductions.txt', 'r') as introductions:
+			dialogues = [x.split(';') for x in introductions.readlines()]
+			self.say(dialogues[i][j])
